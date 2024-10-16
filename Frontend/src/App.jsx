4ec65968783311782f5b7fc58b294ab5f6c40686
Mekin Jemal <mekinjemal999@gmail.com>
2024-10-16 05:53:52 +0300

    Install react js and tailwind css and create the  first template

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className=" text-3xl text-slate-700">Blog Project</div>
+    </>
+  );
+}
+
+export default App;
