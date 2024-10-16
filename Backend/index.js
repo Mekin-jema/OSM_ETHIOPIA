0b5f1765fc339b6ccae41591f08d9c5d829c5551
Mekin Jemal <mekinjemal999@gmail.com>
2024-10-16 13:25:45 +0300

    create test api route

--- Backend/index.js
+++ Backend/index.js
@@ -2,9 +2,15 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import { connectDB } from "./database/connectDB.js";
+import userRouter from "./routes/user.routes.js";
 
 const app = express(); // Create an Express app
 
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.use("/api/user", userRouter);
 connectDB((next) => {
   app.listen(process.env.PORT, () => {
     console.log("Server is running on port 3000");
