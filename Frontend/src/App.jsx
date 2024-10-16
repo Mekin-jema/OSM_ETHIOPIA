252cbdeacc792e3f6ceac2451ef8c73407208df1
Mekin Jemal <mekinjemal999@gmail.com>
2024-10-16 07:40:34 +0300

    Complete the header section

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,10 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home, About, SignIn, SignUp, Dashboard, Project } from "./Pages";
+import Header from "./Components/Header";
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-
+import { Button } from "flowbite-react";
 const Home = () => {
-  return <div>Home</div>;
+  return <Button>Home</Button>;
 };
 
 export default Home;
