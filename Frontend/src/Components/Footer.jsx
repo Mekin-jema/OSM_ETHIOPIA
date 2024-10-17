57ba78dd468adfd223d4ee08899600b23ef2cc44
Mekin Jemal <mekinjemal999@gmail.com>
2024-10-17 16:14:23 +0300

    Complete footer components

--- Frontend/src/Components/Footer.jsx
+++ Frontend/src/Components/Footer.jsx
@@ -1,22 +1,104 @@
 import React from "react";
-import { Footer } from "flowbite-react";
+import { Footer, FooterDivider } from "flowbite-react";
 import { Link } from "react-router-dom";
+import {
+  BsFacebook,
+  BsInstagram,
+  BsGithub,
+  BsTelegram,
+  BsTwitter,
+} from "react-icons/bs";
 const FooterCom = () => {
   return (
     <Footer container className=" border border-t-8 border-teal-500 text-white">
-      <div className="">
-        <div className="">
-          <Link
-            to="/"
-            className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white 
+      <div className="w-full max-w-7xl mx-auto">
+        <div className=" grid w-full justify-between sm:flex md:grid-cols-1">
+          <div className=" mt-5">
+            <Link
+              to="/"
+              className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white 
        "
-          >
-            <span className=" px- py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
-              Mekin's{" "}
-            </span>
-            Blog
-          </Link>
-          
+            >
+              <span className=" px- py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+                Mekin's{" "}
+              </span>
+              Blog
+            </Link>
+          </div>
+          <div className="grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6">
+            <div>
+              <Footer.Title title="About" />
+              <Footer.LinkGroup col>
+                <Footer.Link
+                  href="https/www.1000jsprojects.com"
+                  target="_blank"
+                  rel="noopener noreferrer" // for security
+                >
+                  100 Js projects
+                </Footer.Link>
+                <Footer.Link
+                  href="/about"
+                  target="_blank"
+                  rel="noopener noreferrer" // for security
+                >
+                  Mekin's Blog
+                </Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title="Follow Us" />
+              <Footer.LinkGroup col>
+                <Footer.Link
+                  href="https/www.1000jsprojects.com"
+                  target="_blank"
+                  rel="noopener noreferrer" // for security
+                >
+                  Github repository
+                </Footer.Link>
+                <Footer.Link
+                  href="/about"
+                  target="_blank"
+                  // rel="noopener noreferrer" // for security
+                >
+                  Discord
+                </Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title="Legal" />
+              <Footer.LinkGroup col>
+                <Footer.Link
+                // href="https/www.1000jsprojects.com"
+                // target="_blank"
+                // rel="noopener noreferrer" // for security
+                >
+                  Privacy Policy
+                </Footer.Link>
+                <Footer.Link
+                  href="/about"
+                  target="_blank"
+                  // rel="noopener noreferrer" // for security
+                >
+                  Terms &amp; Conditions
+                </Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+          </div>
+        </div>
+        <Footer.Divider />
+        <div className=" w-full sm:flex sm:items-center sm:justify-between">
+          <Footer.Copyright
+            href="#"
+            by="Mekin's Blog"
+            year={new Date().getFullYear()}
+          />
+          <div className=" flex gap-6 sm:mt-0 mt-4  sm:items-center sm:justify-between">
+            <Footer.Icon href="#" icon={BsFacebook} />
+            <Footer.Icon href="#" icon={BsInstagram} />
+            <Footer.Icon href="#" icon={BsGithub} />
+            <Footer.Icon href="#" icon={BsTelegram} />
+            <Footer.Icon href="#" icon={BsTwitter} />
+          </div>
         </div>
       </div>
     </Footer>
